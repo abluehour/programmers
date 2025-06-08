a7a77275435a45446a59d5f6250654fc4e7f59cb
abluehour <143566075+Jung-YoonSeong@users.noreply.github.com>
2025-06-09 02:53:11 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 4.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    float answer = 0;
+    answer = (float)num1/num2;
+    answer = answer*1000;
+    return answer;
+}
+
